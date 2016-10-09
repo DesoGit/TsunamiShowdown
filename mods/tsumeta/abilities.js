3d5588215aa8a9ba39bf5c2263d9b1536c5485c9
Steven Basileo <DesoGit@users.noreply.github.com>
2016-10-09 17:15:28 -0400

    Attempt to fix Gluttony glitch
    Attempt #2

--- mods/tsumeta/abilities.js
+++ mods/tsumeta/abilities.js
@@ -18,7 +18,102 @@ exports.BattleAbilities = {
         inherit: true,
         onEatItem: function (item, pokemon) {
             if (!pokemon.volatiles['gluttony']) pokemon.addVolatile('gluttony');
-		if item.isBerry = true onEat = onEat * 2;
+		//if item.isBerry = true onEat = onEat * 2; --Failed one-line rendition
+		if item.id === 'sitrusberry' {
+			this.heal(pokemon.maxhp / 2);
+		}
+		if item.id === 'oranberry' {
+			this.heal(pokemon.maxhp + 20);
+		}
+		if item.id === 'figyberry' {
+			inherit: true,
+			this.heal(pokemon.maxhp / 4);
+		}
+		if item.id === 'wikiberry' {
+			inherit: true,
+			this.heal(pokemon.maxhp / 4);
+		}
+		if item.id === 'magoberry' {
+			inherit: true,
+			this.heal(pokemon.maxhp / 4);
+		}
+		if item.id === 'iapapaberry' {
+			inherit: true,
+			this.heal(pokemon.maxhp / 4);
+		}
+		if item.id === 'aguavberry' {
+			inherit: true,
+			this.heal(pokemon.maxhp / 4);
+		}
+		if item.id === 'occaberry' {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+		if item.id === 'passhoberry' {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+		if item.id === 'wacanberry' {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+		if item.id === 'rindoberry' {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+		if item.id === 'yacheberry' {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+		if item.id === 'chopleberry' {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+		if item.id === 'kebiaberry' {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+		if item.id === 'shucaberry' {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+		if item.id === 'cobaberry' {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+		if item.id === 'payapaberry' {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+		if item.id === 'tangaberry' {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+		if item.id === 'chartiberry' {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+		if item.id === 'chilanberry' {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+		if item.id === 'kasibberry' {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+		if item.id === 'habanberry' {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+		if item.id === 'colburberry' {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+		if item.id === 'habiriberry' {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+		
         },
         effect: {
           duration: 2,
