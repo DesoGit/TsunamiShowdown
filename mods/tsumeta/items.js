69d5d38e0d8ecaf8b09aa5baf13c19faadb05364
Steven Basileo <DesoGit@users.noreply.github.com>
2016-10-10 09:54:45 -0400

    remove inherit properties

--- mods/tsumeta/items.js
+++ mods/tsumeta/items.js
@@ -16,154 +16,154 @@ exports.BattleItems = {
      "figyberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			this.heal(pokemon.maxhp / 4);
 		}
     },
  "wikiberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			this.heal(pokemon.maxhp / 4);
 		}
     },
  "magoberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			this.heal(pokemon.maxhp / 4);
 		}
     },
  "iapapaberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			this.heal(pokemon.maxhp / 4);
 		}
     },
  "aguavberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			this.heal(pokemon.maxhp / 4);
 		}
     },
  "occaberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			return this.chainModify(0.25);
 		}
     },
  "passhoberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			return this.chainModify(0.25);
 		}
     },
  "wacanberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			return this.chainModify(0.25);
 		}
     },
  "rindoberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			return this.chainModify(0.25);
 		}
     },
  "yacheberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			return this.chainModify(0.25);
 		}
     },
  "chopleberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			return this.chainModify(0.25);
 		}
     },
  "kebiaberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			return this.chainModify(0.25);
 		}
     },
  "shucaberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			return this.chainModify(0.25);
 		}
     },
  "cobaberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			return this.chainModify(0.25);
 		}
     },
  "payapaberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			return this.chainModify(0.25);
 		}
     },
  "tangaberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			return this.chainModify(0.25);
 		}
     },
  "chartiberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			return this.chainModify(0.25);
 		}
     },
  "chilanberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			return this.chainModify(0.25);
 		}
     },
  "kasibberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			return this.chainModify(0.25);
 		}
     },
  "habanberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			return this.chainModify(0.25);
 		}
     },
  "colburberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			return this.chainModify(0.25);
 		}
     },
  "babiriberry": 
     function (pokemon, ability, item) {
 		if (pokemon.ability === 'gluttony') {
-			inherit: true,
+			//inherit: true,
 			return this.chainModify(0.25);
 		}
     },
