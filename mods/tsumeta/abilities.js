44b881245f1167497f00a95840d3617d73604670
Steven Basileo <DesoGit@users.noreply.github.com>
2016-10-09 22:37:43 -0400

    Attempt to fix typeSup Berries w/ Gluttony

--- mods/tsumeta/abilities.js
+++ mods/tsumeta/abilities.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var scanIfBerry = item.isBerry;
+var typeSuppresantBerries = ['occaberry','wacanberry','passhoberry','chilanberry','chopleberry','yacheberry','rindoberry','kasibberry','kebiaberry','shucaberry','cobaberry','chartiberry','tangaberry','payapaberry','habanberry','colburberry','habiriberry'];
+
+
 exports.BattleAbilities = {
     "defeatist": {
         inherit: true,
@@ -18,104 +22,6 @@ exports.BattleAbilities = {
         inherit: true,
         onEatItem: function (item, pokemon) {
             if (!pokemon.volatiles['gluttony']) pokemon.addVolatile('gluttony');
-		//if item.isBerry = true onEat = onEat * 2; --Failed one-line rendition
-		
-		/*if (item.id === 'sitrusberry') { //yikes to all of this
-			this.heal(pokemon.maxhp / 2);
-		}
-		if (item.id === 'oranberry') {
-			this.heal(pokemon.maxhp + 20);
-		}
-		if (item.id === 'figyberry') {
-			inherit: true,
-			this.heal(pokemon.maxhp / 4);
-		}
-		if (item.id === 'wikiberry') {
-			inherit: true,
-			this.heal(pokemon.maxhp / 4);
-		}
-		if (item.id === 'magoberry') {
-			inherit: true,
-			this.heal(pokemon.maxhp / 4);
-		}
-		if (item.id === 'iapapaberry') {
-			inherit: true,
-			this.heal(pokemon.maxhp / 4);
-		}
-		if (item.id === 'aguavberry') {
-			inherit: true,
-			this.heal(pokemon.maxhp / 4);
-		}
-		if (item.id === 'occaberry') {
-			inherit: true,
-			return this.chainModify(0.25);
-		}
-		if (item.id === 'passhoberry') {
-			inherit: true,
-			return this.chainModify(0.25);
-		}
-		if (item.id === 'wacanberry') {
-			inherit: true,
-			return this.chainModify(0.25);
-		}
-		if (item.id === 'rindoberry') {
-			inherit: true,
-			return this.chainModify(0.25);
-		}
-		if (item.id === 'yacheberry') {
-			inherit: true,
-			return this.chainModify(0.25);
-		}
-		if (item.id === 'chopleberry') {
-			inherit: true,
-			return this.chainModify(0.25);
-		}
-		if (item.id === 'kebiaberry') {
-			inherit: true,
-			return this.chainModify(0.25);
-		}
-		if (item.id === 'shucaberry') {
-			inherit: true,
-			return this.chainModify(0.25);
-		}
-		if (item.id === 'cobaberry') {
-			inherit: true,
-			return this.chainModify(0.25);
-		}
-		if (item.id === 'payapaberry') {
-			inherit: true,
-			return this.chainModify(0.25);
-		}
-		if (item.id === 'tangaberry') {
-			inherit: true,
-			return this.chainModify(0.25);
-		}
-		if (item.id === 'chartiberry') {
-			inherit: true,
-			return this.chainModify(0.25);
-		}
-		if (item.id === 'chilanberry') {
-			inherit: true,
-			return this.chainModify(0.25);
-		}
-		if (item.id === 'kasibberry') {
-			inherit: true,
-			return this.chainModify(0.25);
-		}
-		if (item.id === 'habanberry') {
-			inherit: true,
-			return this.chainModify(0.25);
-		}
-		if (item.id === 'colburberry') {
-			inherit: true,
-			return this.chainModify(0.25);
-		}
-		if (item.id === 'habiriberry') {
-			inherit: true,
-			return this.chainModify(0.25);
-		}*/
-		
-
 switch (item.id) {
     case 'sitrusberry':
         this.heal(pokemon.maxhp / 2);
@@ -146,15 +52,43 @@ switch (item.id) {
     case 'payapaberry':
     case 'tangaberry':
     case 'chartiberry':
-    case 'chilaberry':
+    case 'chilanberry':
     case 'kasibberry':
     case 'habanberry':
     case 'colburberry':
     case 'habiriberry':
         //inherit: true,
-            return this.chainModify(0.25);
+		if (typeSupressantBerries.indexOf('occaberry') > -1 { return this.chainModify(0.25);}
+		if (typeSupressantBerries.indexOf('passhoberry') > -1 { return this.chainModify(0.25);}
+	    	if (typeSupressantBerries.indexOf('wacanberry') > -1 { return this.chainModify(0.25);}
+		if (typeSupressantBerries.indexOf('rindoberry') > -1 { return this.chainModify(0.25);}
+		if (typeSupressantBerries.indexOf('yacheberry') > -1 { return this.chainModify(0.25);}
+		if (typeSupressantBerries.indexOf('chopleberry') > -1 { return this.chainModify(0.25);}
+		if (typeSupressantBerries.indexOf('kebiaberry') > -1 { return this.chainModify(0.25);}
+		if (typeSupressantBerries.indexOf('shucaberry') > -1 { return this.chainModify(0.25);}
+		if (typeSupressantBerries.indexOf('cobaberry') > -1 { return this.chainModify(0.25);}
+		if (typeSupressantBerries.indexOf('payapaberry') > -1 { return this.chainModify(0.25);}
+		if (typeSupressantBerries.indexOf('tangaberry') > -1 { return this.chainModify(0.25);}
+		if (typeSupressantBerries.indexOf('chartiberry') > -1 { return this.chainModify(0.25);}
+		if (typeSupressantBerries.indexOf('chilanberry') > -1 { return this.chainModify(0.25);}
+		if (typeSupressantBerries.indexOf('kasibberry') > -1 { return this.chainModify(0.25);}
+		if (typeSupressantBerries.indexOf('habanberry') > -1 { return this.chainModify(0.25);}
+		if (typeSupressantBerries.indexOf('colburberry') > -1 { return this.chainModify(0.25);}
+		if (typeSupressantBerries.indexOf('habiriberry') > -1 { return this.chainModify(0.25);}
         break;
+	/*case 'custapberry':
+		if (!pokemon.volatiles['custap']) pokemon.addVolatile('custap');
+		effect: {
+		duration: 2,
+		onStart: function (target) {
+		this.add('-start', target, 'ability: Gluttony');
+		},
+			onModifyAtkPriority: 6,
+				onModifySpAPriority: 6,
+		}*/
+		
 }
+		
 
 
 		
