fb9201d029ea8ec63d06d04703f84246b0ef853f
Steven Basileo <DesoGit@users.noreply.github.com>
2016-10-10 00:00:56 -0400

    Attempt to fix Gluttony glitch
    Also adding items.js can't hurt

---
+++ mods/tsumeta/items.js
@@ -0,0 +1,169 @@
+'use strict';
+
+exports.BattleItems = {
+ "sitrusberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			this.heal(pokemon.maxhp / 2);
+		}
+    },
+ "oranberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			this.heal(pokemon.maxhp + 20);
+		}
+    },
+     "figyberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			this.heal(pokemon.maxhp / 4);
+		}
+    },
+ "wikiberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			this.heal(pokemon.maxhp / 4);
+		}
+    },
+ "magoberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			this.heal(pokemon.maxhp / 4);
+		}
+    },
+ "iapapaberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			this.heal(pokemon.maxhp / 4);
+		}
+    },
+ "aguavberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			this.heal(pokemon.maxhp / 4);
+		}
+    },
+ "occaberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+    },
+ "passhoberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+    },
+ "wacanberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+    },
+ "rindoberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+    },
+ "yacheberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+    },
+ "chopleberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+    },
+ "kebiaberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+    },
+ "shucaberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+    },
+ "cobaberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+    },
+ "payapaberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+    },
+ "tangaberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+    },
+ "chartiberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+    },
+ "chilanberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+    },
+ "kasibberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+    },
+ "habanberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+    },
+ "colburberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+    },
+ "habiriberry": 
+    function (pokemon, ability, item) {
+		if (pokemon.ability === 'gluttony') {
+			inherit: true,
+			return this.chainModify(0.25);
+		}
+    },
