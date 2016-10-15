103a11c3b02d8d6ca404d7ed2758bb9d5fb9eb7e
Mystifi <bornfromthemyst@gmail.com>
2016-10-15 17:23:00 -0500

    Try and fix crash

--- mods/tsumeta/moves.js
+++ mods/tsumeta/moves.js
@@ -59,16 +59,15 @@ exports.BattleMovedex = {
                 }
             }
         }, */
-        onTryHit: function (target) {
-                    if (target.move.id === 'Detect') {
-            breaksProtect = false;
-        }
-            else {
-            if (Math.floor(Math.random()*99) < 30) {
-        breaksProtect: true;
+        onTryHit: function (pokemon, target, move) {
+            if (move.id === 'Detect') {
+                this.breaksProtect = false;
+            } else {
+                if (Math.floor(Math.random()*99) < 30) {
+                    this.breaksProtect = true;
+                }
             }
-            }
-        }
+        },
     },
     "toxic": {
         inherit: true,
