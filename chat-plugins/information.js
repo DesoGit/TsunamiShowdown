2545cbb1211923ad476509fa515c7ae23033e1ac
Steven Basileo <DesoGit@users.noreply.github.com>
2016-10-15 20:47:55 -0400

    Create information.js

---
+++ chat-plugins/information.js
@@ -0,0 +1,6 @@
+exports.commands = {
+tsugit: 'tsunamigithub',
+tsunamigithub: function (target, room, user) {
+this.sendReply('Please view our GitHub <a href ="https://github.com/DesoGit/TsunamiPS">here</a>');
+}
+};
